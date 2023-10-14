cb661f0f67287a26f0d57e279610a84912e869ac
Kevin Liu <kevinxo328@gmail.com>
2023-10-14 10:19:59 +0800

    Chore: use ruff as linter

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from routers import openai
--- utils/openai.py
+++ utils/openai.py
@@ -1,16 +1,16 @@
 import openai
 from typing import Union
-from utils.env import *
+import utils.env as env
 
-openai.api_type = OPENAI_API_TYPE
-openai.api_base = OPENAI_API_ENDPOINT
-openai.api_key = OPENAI_API_KEY
-openai.api_version = OPENAI_API_VERSION
+openai.api_type = env.OPENAI_API_TYPE
+openai.api_base = env.OPENAI_API_ENDPOINT
+openai.api_key = env.OPENAI_API_KEY
+openai.api_version = env.OPENAI_API_VERSION
 
 
 def chat_completion(
     messages: list,
-    model: str = GPT35_TURBO_COMPLETIONS_MODEL,
+    model: str = env.GPT35_TURBO_COMPLETIONS_MODEL,
     temperature: Union[int, float] = 0,
 ):
     res = openai.ChatCompletion.create(
